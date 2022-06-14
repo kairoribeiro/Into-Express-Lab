e9d28fde6f95a8de9d13f2b6fe9d732ca839b3fb
Kairo Ribeiro <kairo10@gmail.com>
2022-06-14 12:35:13 -0400

    add structrure

---
+++ data/students-data.js
@@ -0,0 +1,9 @@
+const students = [
+    {text: '', done: true, _id: 123},
+    {text: '', done: false, _id: 456},
+    {text: '', done: false, _id: 789},
+  ]
+  
+  export { 
+      students
+  }
---
+++ server.js
@@ -0,0 +1,34 @@
+// import modules
+
+import express from 'express'
+
+import { students } from './data/students-data.js'
+
+// Create Express app
+
+const app = express()
+
+// Configure the app (app.set)
+
+app.set('view engine', 'ejs')
+
+
+// Mount routes
+
+app.get('/', function(req, res) {
+    res.redirect('/students')
+  })
+  
+  app.get('/todos', function(req, res){
+    res.render('students/index', {
+      students: students
+    })
+  })
+
+
+
+// Tell the app to listen on port 3000
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
